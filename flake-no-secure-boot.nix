ac036a1dcfd1bca5ae9e86e6d3571bb3c890fe69
Leo Newton <leo253@pm.me>
2026-02-08 09:16:14 -0800

    Add NixOS configuration for unstable environment
    This file sets up a NixOS configuration using the latest unstable Nix packages, Determinate Systems, and Home Manager.

---
+++ flake-no-secure-boot.nix
@@ -0,0 +1,43 @@
+{
+  description = "NixOS Unstable Configuration with Determinate Nix";
+
+  inputs = {
+    # 1. Use the latest unstable Nix packages
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    #nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+    
+
+    # 2. Determinate Systems Flake (Improved Nix settings, caching, daemon)
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
+
+    # 3. Home Manager (must match nixpkgs version)
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, determinate, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; }; # Pass inputs to modules
+      modules = [
+        # Your main configuration file
+        ./configuration.nix
+        
+        # The Determinate Nix module (Enables Flakes, Determinate Caching, etc.)
+        determinate.nixosModules.default
+
+        # Home Manager Module (Replaces <home-manager/nixos>)
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          
+          # Pass inputs to home-manager modules so you can use them there too
+          home-manager.extraSpecialArgs = { inherit inputs; };
+        }
+      ];
+    };
+  };
+}
